382b506933f2b54b52b7d8acdc6544e249d373f7
nhnhmhnm <onep5286@gmail.com>
2025-07-10 19:11:32 +0900

    환경변수 설정

---
+++ crawler/config.py
@@ -0,0 +1,14 @@
+DB_CONFIG = {
+    "host": "localhost",
+    "user": "root",
+    "password": "root",
+    "database": "exchange_tracker",
+}
+
+CHROME_DRIVER_PATH = "C:\\chromedriver\\chromedriver.exe"
+
+# 은행
+BANKS = ["ibk"]
+
+# 통화
+CURRENCIES = ["USD"]
