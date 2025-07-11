331ee686704beaf7c5891fb4a101ea54e7d38efb
nhnhmhnm <onep5286@gmail.com>
2025-07-11 16:39:32 +0900

    context manager 사용

--- crawler/utils/DBconnect.py
+++ crawler/utils/DBconnect.py
@@ -2,23 +2,20 @@ import mysql.connector
 from config import DB_CONFIG
 
 
-def insert_rate(bank: str, currency: str, rate: float, timestamp):
-    try:
-        conn = mysql.connector.connect(**DB_CONFIG)
-        cursor = conn.cursor()
+def insert_rate(bank: str, currency: str, rate: float, timestamp) :
+    try :
+        # context manager로 mysql, cursor 열기 (자동 닫힘)
+        with mysql.connector.connect(**DB_CONFIG) as conn :
+            with conn.cursor() as cursor :
+                sql = """
+                    INSERT INTO exchange_rates (bank, currency, rate, timestamp)
+                    VALUES (%s, %s, %s, %s)
+                    ON DUPLICATE KEY UPDATE rate = VALUES(rate)
+                """
 
-        sql = """
-            INSERT INTO exchange_rates (bank, currency, rate, timestamp)
-            VALUES (%s, %s, %s, %s)
-            ON DUPLICATE KEY UPDATE rate = VALUES(rate)
-        """
-        cursor.execute(sql, (bank, currency, rate, timestamp))
-        conn.commit()
+                cursor.execute(sql, (bank, currency, rate, timestamp))
+                conn.commit()
+                print(f"[DB] 저장 성공 : {bank} {currency} {rate} {timestamp}")
 
-        print(f"[DB] 저장 성공 : {bank} {currency} {rate} {timestamp}")
-
-    except Exception as e:
+    except Exception as e :
         print(f"[DB ERROR] {e}")
-    finally:
-        if cursor: cursor.close()
-        if conn: conn.close()
