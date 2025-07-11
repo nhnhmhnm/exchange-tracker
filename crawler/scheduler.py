9601de114ff2b6d9ce88e49f8b585bd2a6bb74c1
nhnhmhnm <onep5286@gmail.com>
2025-07-11 17:05:14 +0900

    파일 위치 변경

--- crawler/scheduler.py
+++ crawler/scheduler.py
@@ -2,9 +2,10 @@ import schedule
 import time
 from main import crawl_ibk
 
+# 10분마다 실행
 schedule.every(10).minutes.do(crawl_ibk)
 
-print("[시작] 기업은행 USD 환율 크롤러 (10분마다 실행)")
+print("[시작] IBK기업은행 USD 환율 크롤러")
 
 crawl_ibk()  # 최초 실행
 
