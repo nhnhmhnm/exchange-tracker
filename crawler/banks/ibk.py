47538e4bb8bda0d4af7f659f545f553e89cebada
nhnhmhnm <onep5286@gmail.com>
2025-07-16 17:14:11 +0900

    driver 예외 처리

--- crawler/banks/ibk.py
+++ crawler/banks/ibk.py
@@ -8,20 +8,22 @@ from datetime import datetime
 from utils.enums import Bank, Currency
 from utils.notice_time import parse_notice_time
 
-def get_ibk(currency: Currency) -> dict | None :
+def get_ibk(currency: Currency) -> dict | None:
     url = "https://www.ibk.co.kr/fxtr/excRateList.ibk?pageId=SM03020100"
 
     options = Options()
     options.add_argument("--headless")
+    
+    driver = None 
 
-    try :
+    try:
         service = Service(executable_path=CHROME_DRIVER_PATH)
         driver = webdriver.Chrome(service=service, options=options)
 
         driver.get(url)
 
         # 고시완료 시각
-        try :
+        try:
             time_tag = driver.find_element(By.CLASS_NAME, "standard")
             time_text = time_tag.text.strip()
             print(f"[INFO] IBK {time_text}")
@@ -29,7 +31,7 @@ def get_ibk(currency: Currency) -> dict | None :
             # 파싱
             timestamp = parse_notice_time(time_text)
 
-        except Exception as e :
+        except Exception as e:
             timestamp = datetime.now()
             print(f"[WARN] IBK 고시완료 시각 파싱 실패 {e}")
 
@@ -38,33 +40,28 @@ def get_ibk(currency: Currency) -> dict | None :
         rows = table.find_elements(By.TAG_NAME, "tr")
         print(f"[INFO] IBK 테이블 로드 성공")
 
-        for row in rows :
+        for row in rows:
             ths = row.find_elements(By.TAG_NAME, "th") # 통화
 
-            if ths and currency.value in ths[0].text :
+            if ths and currency.value in ths[0].text:
                 tds = row.find_elements(By.TAG_NAME, "td") # 매매기준율
 
-                if len(tds) >= 1 :
+                if len(tds) >= 1:
                     rate_text = tds[0].text.strip().replace(",", "")
                     rate = float(rate_text)
                     print(f"[INFO] IBK {currency.value} 매매기준율: {rate}")
 
                     return {
-                        "bank": Bank.IBK.value,
-                        "currency": currency.value,
+                        "bank": Bank.IBK,
+                        "currency": currency,
                         "rate": rate,
                         "timestamp": timestamp
                     }
 
         print(f"[WARN] IBK {currency.value} 데이터를 찾지 못함")
 
-    except Exception as e :
+    except Exception as e:
         print(f"[ERROR] get_ibk 실패: {e}")
 
-    finally :
-        try :
-            driver.quit()
-        except :
-            pass
-
-    return None
+    finally:
+        if driver: driver.quit()
