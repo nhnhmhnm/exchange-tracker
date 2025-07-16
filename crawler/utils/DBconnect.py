8203a0cc2c0cb2827fb6fa5c9d728d77017329db
nhnhmhnm <onep5286@gmail.com>
2025-07-16 17:12:13 +0900

    은행과 통화의 fk 가져오는 함수 추가

--- crawler/utils/DBconnect.py
+++ crawler/utils/DBconnect.py
@@ -1,21 +1,52 @@
 import mysql.connector
 from config import DB_CONFIG
+from utils.enums import Bank, Currency
+
+def get_foreign_key_id(cursor, table: str, code: str) -> int:
+    sql = f"SELECT id FROM {table} WHERE code = %s"
+    cursor.execute(sql, (code,))
+    result = cursor.fetchone()
+    if result is None:
+        raise ValueError(f"{table}에서 code='{code}'를 찾을 수 없습니다.")
+    return result[0]
+
+def insert_rate(bank: Bank, currency: Currency, rate: float, timestamp):
+    try:
+        with mysql.connector.connect(**DB_CONFIG) as conn:
+            with conn.cursor() as cursor:
+                # bank_id, currency_id 조회
+                bank_id = get_foreign_key_id(cursor, "banks", bank.value)
+                currency_id = get_foreign_key_id(cursor, "currencies", currency.value)
 
 
-def insert_rate(bank: str, currency: str, rate: float, timestamp) :
-    try :
-        # context manager로 mysql, cursor 열기 (자동 닫힘)
-        with mysql.connector.connect(**DB_CONFIG) as conn :
-            with conn.cursor() as cursor :
+                # INSERT
                 sql = """
-                    INSERT INTO exchange_rates (bank, currency, rate, timestamp)
+                    INSERT INTO exchange_rates (bank_id, currency_id, rate, timestamp)
                     VALUES (%s, %s, %s, %s)
                     ON DUPLICATE KEY UPDATE rate = VALUES(rate)
                 """
-
-                cursor.execute(sql, (bank, currency, rate, timestamp))
+                cursor.execute(sql, (bank_id, currency_id, rate, timestamp))
                 conn.commit()
-                print(f"[DB] 저장 성공 : {bank} {currency} {rate} {timestamp}")
 
-    except Exception as e :
+                print(f"[DB] 저장 성공 : {bank.value} {currency.value} {rate} {timestamp}")
+
+    except Exception as e:
         print(f"[DB ERROR] {e}")
+
+# def insert_rate(bank: str, currency: str, rate: float, timestamp) :
+#     try:
+#         # context manager로 mysql, cursor 열기 (자동 닫힘)
+#         with mysql.connector.connect(**DB_CONFIG) as conn:
+#             with conn.cursor() as cursor:
+#                 sql = """
+#                     INSERT INTO exchange_rates (bank, currency, rate, timestamp)
+#                     VALUES (%s, %s, %s, %s)
+#                     ON DUPLICATE KEY UPDATE rate = VALUES(rate)
+#                 """
+
+#                 cursor.execute(sql, (bank, currency, rate, timestamp))
+#                 conn.commit()
+#                 print(f"[DB] 저장 성공 : {bank} {currency} {rate} {timestamp}")
+
+#     except Exception as e:
+#         print(f"[DB ERROR] {e}")
