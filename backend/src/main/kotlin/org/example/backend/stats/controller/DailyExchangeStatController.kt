1a580b98f52ffe79cb8423e90ea6c6b31836581b
nhnhmhnm <onep5286@gmail.com>
2025-07-16 17:26:11 +0900

    controller 생성 날짜를 기반으로 통계

--- backend/src/main/kotlin/org/example/backend/stats/controller/DailyExchangeStatController.kt
+++ backend/src/main/kotlin/org/example/backend/stats/controller/DailyExchangeStatController.kt
@@ -1,20 +1,16 @@
-package org.example.backend.controller
+package org.example.backend.stats.controller
 
-import org.example.backend.dto.DailyExchangeStatResponse
-import org.example.backend.service.DailyExchangeStatService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.example.backend.stats.service.DailyExchangeStatService
+import org.springframework.web.bind.annotation.*
 import java.time.LocalDate
 
 @RestController
 @RequestMapping("/api/v1/daily-exchange")
-class DailyStatController(
+class DailyExchangeStatController(
     private val dailyExchangeStatService: DailyExchangeStatService
 ) {
-    @GetMapping
-    fun getStats(@RequestParam date: LocalDate): {
-        return dailyExchangeStatService.getStatsByDate(date)
+    @PostMapping
+    fun getStats(@RequestParam date: LocalDate) {
+        dailyExchangeStatService.DailyStats(date)
     }
 }
