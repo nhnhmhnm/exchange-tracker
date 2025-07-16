208f415c2c244005bdd04a04b11d88b5777857ec
nhnhmhnm <onep5286@gmail.com>
2025-07-16 17:25:34 +0900

    entity 생성

--- backend/src/main/kotlin/org/example/backend/stats/domain/DailyExchangeStat.kt
+++ backend/src/main/kotlin/org/example/backend/stats/domain/DailyExchangeStat.kt
@@ -1,14 +1,16 @@
-package org.example.backend.domain
+package org.example.backend.stats.domain
 
 import java.math.BigDecimal
 import java.time.LocalDateTime
 
 data class DailyExchangeStat(
-    val bank: String,
-    val currency: String,
-    val highestRate: BigDecimal,
+    val bankId: Long,
+    val currencyId: Long,
+
     val lowestRate: BigDecimal,
     val medianRate: BigDecimal,
+    val highestRate: BigDecimal,
+
     val startTime: LocalDateTime,
     val endTime: LocalDateTime
 )
