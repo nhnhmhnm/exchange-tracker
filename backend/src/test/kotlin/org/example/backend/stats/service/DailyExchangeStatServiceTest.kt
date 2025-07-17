6a9664dcf66f589a73e4be5b27ee5fd564798a6c
nhnhmhnm <onep5286@gmail.com>
2025-07-17 17:21:48 +0900

    service 테스트 코드 (실패)

--- backend/src/test/kotlin/org/example/backend/stats/service/DailyExchangeStatServiceTest.kt
+++ backend/src/test/kotlin/org/example/backend/stats/service/DailyExchangeStatServiceTest.kt
@@ -1,6 +1,7 @@
 package org.example.backend.stats.service
 
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
 import io.mockk.*
 import org.example.backend.stats.dto.DailyExchangeStatResponse
 import org.example.backend.stats.repository.DailyExchangeStatRepository
@@ -9,6 +10,8 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 
 class DailyExchangeStatServiceTest : BehaviorSpec({
+//    val repository = mockk<DailyExchangeStatRepository>(relaxed = true)
+//    val service =DailyExchangeStatService(repository)
     lateinit var repository: DailyExchangeStatRepository
     lateinit var service: DailyExchangeStatService
 
@@ -17,39 +20,48 @@ class DailyExchangeStatServiceTest : BehaviorSpec({
         service = DailyExchangeStatService(repository)
     }
 
-    given("해당 날짜의 환율 데이터가 존재할 때") {
-        val date = LocalDate.of(2025, 7, 16)
+    Given("해당 날짜의 환율 데이터가 존재할 때") {
+        val date = LocalDate.of(2025, 7, 14)
         val stat = DailyExchangeStatResponse(
-                bankId = 1,
-                currencyId = 1,
-                lowestRate = BigDecimal("931.10"),
-                medianRate = BigDecimal("932.20"),
-                highestRate = BigDecimal("933.30"),
-                startTime = LocalDateTime.of(2025, 7, 14, 9, 0),
-                endTime = LocalDateTime.of(2025, 7, 14, 18, 0)
+            bankId = 1,
+            currencyId = 1,
+            lowestRate = BigDecimal("931.10"),
+            medianRate = BigDecimal("932.20"),
+            highestRate = BigDecimal("933.30"),
+            startTime = LocalDateTime.of(2025, 7, 14, 9, 0),
+            endTime = LocalDateTime.of(2025, 7, 14, 18, 0)
         )
 
-        every { repository.getStatByDate(date) } returns stat
-        every { repository.saveStat(any()) } just Runs
+        val slot = slot<DailyExchangeStatResponse>()
 
-        `when`("서비스가 실행되면") {
-            service.findStats(date)
+        every { repository.getStatByDate(any()) } returns stat
+        every { repository.saveStat(capture(slot)) } just Runs
 
-            then("계산된 통계 데이터를 저장해야 한다") {
-                verify(exactly = 1) { repository.saveStat(any()) }
+        When("서비스가 실행되면") {
+            service.getStat(date)
+
+            Then("계산된 통계 데이터를 저장해야 한다") {
+                slot.isCaptured shouldBe true
+                slot.captured.bankId shouldBe 1
+                slot.captured.currencyId shouldBe 1
+                slot.captured.lowestRate shouldBe BigDecimal("931.10")
+                slot.captured.medianRate shouldBe BigDecimal("932.20")
+                slot.captured.highestRate shouldBe BigDecimal("933.30")
+                slot.captured.startTime shouldBe LocalDateTime.of(2025, 7, 14, 9, 0)
+                slot.captured.endTime shouldBe LocalDateTime.of(2025, 7, 14, 18, 0)
             }
         }
     }
 
-    given("해당 날짜에 환율 데이터가 존재하지 않을 때") {
-        val date = LocalDate.of(2025, 7, 15)
+    Given("해당 날짜에 환율 데이터가 존재하지 않을 때") {
+        val date = LocalDate.of(2025, 7, 17)
 
-        every { repository.getStatByDate(date)} returns null
+        every { repository.getStatByDate(date) } returns null
 
-        `when`("서비스를 실행하면") {
-            service.findStats(date)
+        When("서비스를 실행하면") {
+            service.getStat(date)
 
-            then("저장 로직이 호출되지 않아야 한다") {
+            Then("저장 로직이 호출되지 않아야 한다") {
                 verify(exactly = 0) { repository.saveStat(any()) }
             }
         }
